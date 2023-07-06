3c42bbe9cb817f5e2ddec8d2a1f636c02f471f2b
AlexMcBex <alessiobelli94@gmail.com>
2023-07-06 11:15:30 -0400

    connected to weather API

---
+++ CLIENT/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React, { useEffect } from 'react';
+
+// COMPONENTS
+import Header from './components/shared/Header'
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Header />
+     <h1>Weatheroo</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ CLIENT/src/api/weather.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+
+const weather = async(query) =>{
+  const axios = require('axios');
+
+  const options = {
+    method: 'GET',
+    url: 'https://weatherapi-com.p.rapidapi.com/current.json',
+    params: {q: query},
+    headers: {
+      'X-RapidAPI-Key': '687ce345e8msh4527dea48c7421ap1fd768jsn5831cc069d3d',
+      'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+    }
+  };
+  
+  try {
+    const response = await axios.request(options);
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+weather('firenze')
